d5896971a72322e6d6e11f4fb2385c82fb903616
Aida Bourdis <aida.lucie@gmail.com>
2021-02-19 16:30:57 +0000

    frog moving around grid, edges of grid updated so frog does not move beyond borders

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ function init() {
   const grid = document.querySelector('.grid') // grab grid
   
   const width = 10 // grid size
-  const height = 10
+  const height = 20
   const cellCount = width * height // number of cells
   const cells = []
   // console.log('Grid>>>', grid)
@@ -21,14 +21,45 @@ function init() {
     }
     addFrog(frogStartPosition)
   }
-  console.log('frog>>>', grid)
+  // console.log('frog>>>', grid)
 
-// * Add Frog to grid
-function addFrog(position) { //position makes it reusable, you could add current position or start position or a random index for a random position.
-  cells[position].classList.add(frogClass) //add css Frog class
-}
+  // * Add Frog to grid
+  function addFrog(position) { //position makes it reusable, you could add current position or start position or a random index for a random position.
+    cells[position].classList.add(frogClass) //add css Frog class
+  }
+
+  // * Remove Frog from the grid --> so that you don't get a bunch of cats on the screen evertime you try to move it.
+  function removeFrog(position) { 
+    cells[position].classList.remove(frogClass)
+  }
+
+  // * Move Frog
+
+  function handleKeyDown(event) {
+    const key = event.keyCode // recognises that you are pressing keys and which key it is
+
+    removeFrog(frogCurrentPosition) // 1. remove cat
 
 
+    // 2. check  what key has been pressed to decide where cat should go
+    if (key === 39 /*right*/ && frogCurrentPosition % width !== width - 1) { 
+      frogCurrentPosition++
+    } else if (key === 37 /*left*/&& frogCurrentPosition % width !== 0) {
+      frogCurrentPosition--
+    } else if (key === 38 /*up*/ && frogCurrentPosition >= width) {
+      frogCurrentPosition -= width
+    } else if (key === 40 /*down*/ && frogCurrentPosition + width <= width * height - 1) {
+      frogCurrentPosition += width
+    } else {
+      console.log('INVALID KEY')
+    }
+    
+    // 3. add cat to new position
+    addFrog(frogCurrentPosition)
+  }
+
+
+  document.addEventListener('keydown', handleKeyDown)
 
   createGrid() 
   console.log('cells>>>', grid)
